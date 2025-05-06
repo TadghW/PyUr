eb28a5af89265b8aec9b1d670c85f3bb962fb163
Tadgh Wagstaff <tadgh.wagstaff@paradoxinteractive.com>
2025-05-06 16:38:49 +0200

    making a more serious attempt at a game state and drawing system

--- PyUr.py
+++ PyUr.py
@@ -1,114 +1,54 @@
 import random
+from graphics import compose_screen
 
-round = 1
-player = 1
-turn = 1
-turn_repeat_flag = False
+turn_info = {}
+turn_info["round"] = 1
+turn_info["active_player"] = 1
+turn_info["consecutive_turn_count"] = 1
+turn_info["turn_repeat_flag"] = False
+turn_info["status"] = ""
+turn_info["last_roll"] = []
+turn_info["movement_points"] = 0
+turn_info["p1_turn_count"] = 0
+turn_info["p2_turn_count"] = 0
 
-p1_pieces = {}
-p1_pieces["A"]["position"] = 0
-p1_pieces["B"]["position"] = 0
-p1_pieces["C"]["position"] = 0
-p1_pieces["D"]["position"] = 0
-p1_pieces["E"]["position"] = 0
-p1_pieces["F"]["position"] = 0
-p1_pieces["G"]["position"] = 0
-p1_turn_number = 0
+pieces = {}
+pieces["p1_pieces"]["A"]["position"] = 0
+pieces["p1_pieces"]["B"]["position"] = 0
+pieces["p1_pieces"]["C"]["position"] = 0
+pieces["p1_pieces"]["D"]["position"] = 0
+pieces["p1_pieces"]["E"]["position"] = 0
+pieces["p1_pieces"]["F"]["position"] = 0
+pieces["p1_pieces"]["G"]["position"] = 0
+pieces["p2_pieces"]["a"]["position"] = 0
+pieces["p2_pieces"]["b"]["position"] = 0
+pieces["p2_pieces"]["c"]["position"] = 0
+pieces["p2_pieces"]["d"]["position"] = 0
+pieces["p2_pieces"]["e"]["position"] = 0
+pieces["p2_pieces"]["f"]["position"] = 0
+pieces["p2_pieces"]["g"]["position"] = 0
 
-p2_pieces = {}
-p2_pieces["a"]["position"] = 0
-p2_pieces["b"]["position"] = 0
-p2_pieces["c"]["position"] = 0
-p2_pieces["d"]["position"] = 0
-p2_pieces["e"]["position"] = 0
-p2_pieces["f"]["position"] = 0
-p2_pieces["g"]["position"] = 0
-p2_turn_number = 0
-p2_private_1 = ["", "", "", "*"]
-p2_private_2 = ["", "*"]
+private_path_1_default = ["", "", "", "*"]
+private_path_2_default = ["", "*"]
+shared_path_default = ["", "", "", "*", "", "", "", ""]
 
-shared_path = ["", "", "", "*", "", "", "", ""]
-status = ""
-last_roll = []
-movement_points = 0
+board = {}
+board["p1_sideline"] = []
+board["p1_private_1"] = private_path_1_default
+board["p1_private_2"] = private_path_2_default
+board["p2_sideline"] = []
+board["p2_private_1"] = private_path_1_default
+board["p2_private_2"] = private_path_2_default
+board["shared"] = shared_path_default
 
-
-def compose_sideline(sideline: list[str]) -> str:
-    sideline_str = ""
-    for sideline_content in sideline:
-        sideline_str += f"{sideline_content} "
-    return sideline_str
-
-
-def compose_row(row: list[str]) -> str:
-    row_str = ""
-    for space_content in row:
-        if space_content == "*":
-            row_str += "[ * ]"
-        elif space_content == "":
-            row_str += "[   ]"
-        else:
-            row_str += f" [ {space_content} ] "
-    return row_str
+game_state = {}
+game_state["turn_info"] = turn_info
+game_state["pieces"] = pieces
+game_state["board"] = board
 
 
-def compose_private(private_1: list[str], private_2: list[str]) -> str:
-    # Pieces move right to left on private rows
-    # We're representing those two segments at lists
-    # So we're going to reverse them
-    reversed_private_1 = compose_row(list(reversed(private_1)))
-    reversed_private_2 = compose_row(list(reversed(private_2)))
-    
-    return f"{reversed_private_1}          {reversed_private_2}"
-
-
-def distribute_pieces(pieces, sideline, private_1, private_2, shared) -> list[list]:
-    
-    for piece in pieces:
-        pos = piece["position"]
-        if pos == 0:
-            sideline.append(piece)
-        elif 1 <= pos <= 4:
-            private_1[pos - 1] = piece
-        elif 5 <= pos <= 12:
-            shared[pos - 5] = piece
-        elif 13 <= pos <= 14:
-            private_2[pos - 13] = piece
-
-
-def compose_screen() -> str:
-
-    space_line = ""
-    title = "*** PyUr ***"
-
-    p1_sideline = []
-    p1_private_1 = ["", "", "", "*"]
-    p1_private_2 = ["", "*"]
-    p2_sideline = []
-    p2_private_1 = ["", "", "", "*"]
-    p2_private_2 = ["", "*"]
-    shared = ["", "", "", "*", "", "", "", ""]
-
-    distribute_pieces(p1_pieces, p1_sideline, p1_private_1, p1_private_2, shared)
-    distribute_pieces(p2_pieces, p2_sideline, p2_private_1, p2_private_2, shared)
-                        
-    for piece in p2_pieces:
-        if piece["position"] == 0:
-            p1_sideline.append(piece)
-
-    sidelines_1 = compose_sideline(p1_sideline)
-    private_1 = compose_private(p1_private_1, p1_private_2)
-    shared = compose_row(shared_path)
-    private_2 = compose_private(p2_private_1, p2_private_2)
-    sidelines_2 = compose_sideline(p2_sideline)
-    turn_count = f"Player {player} turn {turn}"
-    roll = f"Rolled {last_roll[0]}, {last_roll[1]}, and {last_roll[2]}. You move {movement_points} spaces"
-    lines = [space_line, title, space_line, sidelines_1, private_1, shared, private_2, sidelines_2, space_line, turn_count, roll, status]
-    return "\n".join(lines)
-
-
-def draw_screen() -> None:
-    print(compose_screen())
+def render_screenspace() -> None:
+    print(compose_screen(game_state))
 
 
 def roll_dice() -> None:
@@ -118,13 +58,12 @@ def roll_dice() -> None:
     for roll in range(num_rolls):
         throw = random.choice(sides)
         result.append(throw)
-    last_roll = result
-
+    game_state["last_roll"] = result
+    game_state["movement_points"] = list(sum(result))
 
 def request_move() -> None:
     
-    status = "What piece would you like to move?"
-    selected_piece = input('Piece: ')
+    selected_piece = input('What piece would you like to move: ')
     # All move logic will now take place with the player pieces dicts
     # Drawing the board is decoupled
     # Must check the validity of moves and piece positions during moves
---
+++ graphics.py
@@ -0,0 +1,93 @@
+from types import Tuple
+
+private_path_1_default = ["", "", "", "*"]
+private_path_2_default = ["", "*"]
+shared_path_default = ["", "", "", "*", "", "", "", ""]
+
+
+def distribute_pieces(board: dict, pieces: dict) -> dict:
+    
+    # This function does no state checking, it just distributes pieces
+
+    for piece in pieces["p1_pieces"]:
+        pos = piece["position"]
+        if pos == 0:
+            board["p1_sideline"].append(piece)
+        elif 1 <= pos <= 4:
+            board["p1_private_1"][pos - 1] = piece
+        elif 5 <= pos <= 12:
+            board["shared"][pos - 5] = piece
+        elif 13 <= pos <= 14:
+            board["p1_private_2"][pos - 13] = piece
+
+    for piece in pieces["p2_pieces"]:
+        pos = piece["position"]
+        if pos == 0:
+            board["p2_sideline"].append(piece)
+        elif 1 <= pos <= 4:
+            board["p2_private_1"][pos - 1] = piece
+        elif 5 <= pos <= 12:
+            board["shared"][pos - 5] = piece
+        elif 13 <= pos <= 14:
+            board["p2_private_2"][pos - 13] = piece
+
+    return board
+
+
+def draw_sideline(sideline: list[str]) -> str:
+    sideline_str = ""
+    for sideline_content in sideline:
+        sideline_str += f"{sideline_content} "
+    return sideline_str
+
+
+def draw_row(row: list[str]) -> str:
+    row_str = ""
+    for space_content in row:
+        if space_content == "*":
+            row_str += "[ * ]"
+        elif space_content == "":
+            row_str += "[   ]"
+        else:
+            row_str += f" [ {space_content} ] "
+    return row_str
+
+
+def draw_private(private_1: list[str], private_2: list[str]) -> str:
+    # Pieces move right to left on private rows
+    # We conceptualise them left to right in the code
+    # And reverse them on print.
+    reversed_private_1 = draw_row(list(reversed(private_path_1_default)))
+    reversed_private_2 = draw_row(list(reversed(private_path_2_default)))
+    return f"{reversed_private_1}          {reversed_private_2}"
+
+
+def compose_screen(game_state: dict) -> str:
+
+    space = ""
+    title = "*** PyUr ***"
+
+    game_state["board"] = distribute_pieces(game_state["pieces"],
+                                            game_state["board"])
+    
+    sidelines_1 = draw_sideline(game_state["board"]["p1_sideline"])
+    private_1 = draw_private(game_state["board"]["p1_private_1"],
+                             game_state["board"]["p1_private_2"])
+    
+    shared = draw_row(game_state["board"]["shared_path"])
+    
+    private_2 = draw_private(game_state["board"]["p2_private_1"],
+                             game_state["board"]["p2_private_2"])
+    sidelines_2 = draw_sideline(game_state["board"]["p2_sideline"])
+    
+    round = f"Round {game_state["round"]}"
+    turn_count = f"Player {game_state["active_player"]}, turn {game_state["consecutive_turn_count"""]}"
+    
+    roll = f"You rolled {game_state["last_roll"][0]}, {game_state["last_roll"][1]}, and {game_state["last_roll"][2]}. You net {game_state["movement_points"]} movement points."
+    
+    status = game_state["board"]["status"]
+
+    lines = [space, title, space, sidelines_1, private_1, shared, private_2,
+             sidelines_2, space, round, turn_count, roll, status]
+    
+    return "\n".join(lines)
